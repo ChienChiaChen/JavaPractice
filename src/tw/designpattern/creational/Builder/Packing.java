22c2cb1e367b54392f712defce1ca2d0ca5296e7
chiachen <s124202468@gmail.com>
2016-12-19 23:06:01 +0800

    builder pattern

---
+++ src/tw/designpattern/creational/Builder/Packing.java
@@ -0,0 +1,8 @@
+package tw.designpattern.creational.Builder;
+
+/**
+ * Created by chiachen on 2016/11/24.
+ */
+public interface Packing {
+    public String pack();
+}
---
+++ src/tw/designpattern/creational/Builder/Wrapper.java
@@ -0,0 +1,11 @@
+package tw.designpattern.creational.Builder;
+
+/**
+ * Created by chiachen on 2016/11/24.
+ */
+public class Wrapper implements Packing {
+    @Override
+    public String pack() {
+        return "Wrapper";
+    }
+}
