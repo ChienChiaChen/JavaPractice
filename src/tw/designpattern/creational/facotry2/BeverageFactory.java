de7baf24e8cea377c9cded0db09fc6e628ae0679
chiachen <s124202468@gmail.com>
2016-12-19 23:06:31 +0800

    factory pattern 2

---
+++ src/tw/designpattern/creational/facotry2/BeverageFactory.java
@@ -0,0 +1,8 @@
+package tw.designpattern.creational.facotry2;
+
+/**
+ * Created by chiachen on 2016/11/6.
+ */
+public interface BeverageFactory {
+    BeverageProvide CreateBeverage();
+}
---
+++ src/tw/designpattern/creational/facotry2/BeverageProvide.java
@@ -0,0 +1,10 @@
+package tw.designpattern.creational.facotry2;
+
+/**
+ * Created by chiachen on 2016/11/6.
+ */
+public interface BeverageProvide {
+    void addMaterial();
+    void brew();
+    void PouredCup();
+}
