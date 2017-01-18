db7b9f61bd64e5a05a1d57413f3324d05dd30279
chiachen <s124202468@gmail.com>
2017-01-18 11:46:58 +0800

    Decorator pattern

---
+++ src/tw/designpattern/creational/Decorator/RedHair.java
@@ -0,0 +1,16 @@
+package tw.designpattern.creational.Decorator;
+
+/**
+ * Created by chiachen on 2017/1/18.
+ */
+public class RedHair extends AvatarDecorator {
+
+    public RedHair(Avatar av) {
+        super(av);
+    }
+
+    @Override
+    public String describe() {
+        return super.describe()+"RedHair";
+    }
+}
---
+++ src/tw/designpattern/creational/Decorator/Worrior.java
@@ -0,0 +1,13 @@
+package tw.designpattern.creational.Decorator;
+
+/**
+ * Created by chiachen on 2017/1/18.
+ */
+public class Worrior  implements Avatar{
+
+
+    @Override
+    public String describe() {
+        return null;
+    }
+}
