9a4e0bc8435a01ea70b7fa34cc438ec533213e97
Jason_Chien <Jason_Chien@PerfectCorp.com>
2017-05-03 18:15:38 +0800

    Decorator 2

---
+++ src/tw/designpattern/structural/Decorator2/Main.java
@@ -0,0 +1,52 @@
+package tw.designpattern.structural.Decorator2;
+
+/**
+ * Created by Jason_Chien on 2017/5/3.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new RedShapeDecorator(new Triangle()).draw();
+        new RedShapeDecorator(new Rectangle()).draw();
+    }
+}
+interface Shape{
+    void draw();
+}
+
+abstract class ShapeDecorator implements Shape {
+    protected Shape shape;
+
+    public ShapeDecorator(Shape shape) {
+        this.shape = shape;
+    }
+
+    @Override
+    public void draw() {
+        shape.draw();
+    }
+}
+
+class Rectangle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("draw : Rectangle");
+    }
+}
+class Triangle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("draw : Triangle");
+    }
+}
+
+class RedShapeDecorator extends ShapeDecorator {
+    public RedShapeDecorator(Shape shape) {
+        super(shape);
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+        System.out.println("plus: RedShapeDecorator");
+    }
+}
