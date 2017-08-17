0f72be097189efb3c779b4ea261884250e356594
Jason_chien <s124202468@gmail.com>
2017-08-18 00:11:36 +0800

    Test-1

--- src/tw/designpattern/delegate/main.java
+++ src/tw/designpattern/delegate/main.java
@@ -5,6 +5,6 @@ public class main {
         BusinessDelegate businessDelegate = new BusinessDelegate();
         businessDelegate.setServiceType("EJB");
         Client client = new Client(businessDelegate);
-        client.doTask();
+        client.doTask();//
     }
 }
