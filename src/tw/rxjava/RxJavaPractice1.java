b5165189eed64981cda9751195e0a4f35a189182
Jason_chien <s124202468@gmail.com>
2017-08-30 15:47:47 +0800

    RxJava lesson2 "Just","from" and "create" were same means

--- src/tw/rxjava/RxJavaPractice1.java
+++ src/tw/rxjava/RxJavaPractice1.java
@@ -40,24 +40,35 @@ public class RxJavaPractice1 {
             public void onError(Throwable e) {//onclick3
                 System.out.println( "subscriber Error");
             }
+
+//            @Override
+//            public void onStart() {
+//                super.onStart();
+//            }
         };
 
 
         //Button
-        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("Hello");
-                subscriber.onNext("Hi");
-                subscriber.onNext("Aloha");
-                subscriber.onCompleted();
-            }
-        });
+//        Case1
+//        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
+//            @Override
+//            public void call(Subscriber<? super String> subscriber) {
+//                subscriber.onNext("Hello");
+//                subscriber.onNext("Hi");
+//                subscriber.onNext("Aloha");
+//                subscriber.onCompleted();
+//            }
+//        });
 
         //Button.setOnClickListener
-        observable.subscribe(observer);
+//        observable.subscribe(observer);
 
+//        Case2
+//        Observable observable = Observable.just("Hello", "Hi", "Alo");
+//        observable.subscribe(observer);
 
-
+//        Case3
+        Observable observable = Observable.from(new String[]{"Hello", "Hi", "Aloha"});
+        observable.subscribe(subscriber);
     }
 }
