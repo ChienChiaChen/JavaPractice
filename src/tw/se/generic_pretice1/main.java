f667719e1d8c407aab1d2d7f250bff7b7bb57b6e
JasonChien <s124202468@gmail.com>
2017-10-19 09:56:58 +0800

    Format it

--- src/tw/se/generic_pretice1/main.java
+++ src/tw/se/generic_pretice1/main.java
@@ -7,28 +7,26 @@ package tw.se.generic_pretice1;
 public class main {
 
 
-    // 泛型方法 printArray
-    public static < E > void printArray( E[] inputArray ) {
-        // 输出数组元素
-        for ( E element : inputArray ){
-            System.out.print(""+element+"\t");
+    public static <E> void printArray(E[] inputArray) {
+        for (E element : inputArray) {
+            System.out.print("" + element + "\t");
         }
         System.out.println();
     }
 
-    public static void main( String args[] ) {
-        Integer[] intArray = { 1, 2, 3, 4, 5 };
-        Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
-        Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+    public static void main(String args[]) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
+        Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
 
-        System.out.println( "Int:" );
-        printArray( intArray  );
+        System.out.println("Int:");
+        printArray(intArray);
 
-        System.out.println( "\ndouble:" );
-        printArray( doubleArray );
+        System.out.println("\ndouble:");
+        printArray(doubleArray);
 
-        System.out.println( "\nchar:" );
-        printArray( charArray );
+        System.out.println("\nchar:");
+        printArray(charArray);
     }
 }
 
