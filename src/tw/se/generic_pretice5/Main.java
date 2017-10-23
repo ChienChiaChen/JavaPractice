3f079e13a6bf4d76200523e0655b51a07c91bd7e
JasonChien <s124202468@gmail.com>
2017-10-23 23:05:54 +0800

    generic_practice5 -> Generic method

--- src/tw/se/generic_pretice5/Main.java
+++ src/tw/se/generic_pretice5/Main.java
@@ -2,10 +2,19 @@ package tw.se.generic_pretice5;
 
 public class Main {
     public static void main(String[] args) {
-        FruitGenerator generator = new FruitGenerator();
-        System.out.println(generator.next());
-        System.out.println(generator.next());
-        System.out.println(generator.next());
-        System.out.println(generator.next());
+//        FruitGenerator generator = new FruitGenerator();
+//        System.out.println(generator.next());
+//        System.out.println(generator.next());
+//        System.out.println(generator.next());
+//        System.out.println(generator.next());
+
+        out("findingsea");
+        out(123);
+        out(11.11);
+        out(true);
+    }
+
+    private static <T> void out(T t) {
+        System.out.println(t);
     }
 }
