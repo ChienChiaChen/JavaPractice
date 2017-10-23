e2422aecb631a44c4f11c92acce2c72c02ac3c1c
JasonChien <s124202468@gmail.com>
2017-10-23 23:10:27 +0800

    generic_practice5 -> Generic method-1

--- src/tw/se/generic_pretice5/Main.java
+++ src/tw/se/generic_pretice5/Main.java
@@ -8,13 +8,22 @@ public class Main {
 //        System.out.println(generator.next());
 //        System.out.println(generator.next());
 
-        out("findingsea");
-        out(123);
-        out(11.11);
-        out(true);
+//        out("findingsea");
+//        out(123);
+//        out(11.11);
+//        out(true);
+
+        out("findingsea", 123, 11.11, true);
+
     }
 
     private static <T> void out(T t) {
         System.out.println(t);
+    }
+
+    public static <T> void out(T... args) {
+        for (T t : args) {
+            System.out.println(t);
+        }
     }
 }
