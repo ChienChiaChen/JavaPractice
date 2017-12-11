5bf02a174efd63b5404a21484834e2829fde91c7
JasonChien <s124202468@gmail.com>
2017-12-12 00:12:20 +0800

    rxjava_1 try dispose

--- src/tw/rxjava/__1/Client.java
+++ src/tw/rxjava/__1/Client.java
@@ -16,27 +16,37 @@ public class Client {
 //                void onError(@NonNull Throwable var1);
 //                void onComplete();
 
+                System.out.println("1 Go");
                 observableEmitter.onNext(1);
+                System.out.println("2 Go");
                 observableEmitter.onNext(2);
+                System.out.println("3 Go");
                 observableEmitter.onNext(3);
+                System.out.println("4 Go");
                 observableEmitter.onNext(4);
                 observableEmitter.onComplete();
-
             }
         }).subscribe(new Observer<Integer>() {
+            int i =0;
+            Disposable mDisposable;
+
             @Override
             public void onSubscribe(Disposable disposable) {
-
+                mDisposable = disposable;
             }
 
             @Override
             public void onNext(Integer integer) {
+                i++;
+                if (i == 2) {
+                    this.mDisposable.dispose();
+                }
                 System.out.println(integer);
             }
 
             @Override
             public void onError(Throwable throwable) {
-
+                System.out.println(throwable.getMessage());
             }
 
             @Override
